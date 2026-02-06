02cdf82a11406025b8287a2fb51b9c51d9d919eb
Alif024 <aleefrock12345@gmail.com>
2026-02-06 19:21:19 +0700

    add navbar and config theme

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,20 @@
-const body = document.body;
+const root = document.documentElement;
 const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme) {
   // user เคยเลือกเอง
-  body.dataset.theme = savedTheme;
+  root.dataset.theme = savedTheme;
 } else {
   // default = ตามระบบ
   const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
     : "light";
-  body.dataset.theme = systemTheme;
+  root.dataset.theme = systemTheme;
 }
 
 document.getElementById("toggleTheme").onclick = () => {
-  const newTheme = document.body.dataset.theme === "dark" ? "light" : "dark";
-
-  document.body.dataset.theme = newTheme;
+  const newTheme = root.dataset.theme === "dark" ? "light" : "dark";
+  root.dataset.theme = newTheme;
   localStorage.setItem("theme", newTheme);
 };
 
