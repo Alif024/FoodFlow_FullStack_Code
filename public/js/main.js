4736e7222e6ab25422dece5c046816a2c6708343
Alif024 <aleefrock12345@gmail.com>
2026-02-07 21:22:27 +0700

    ๒

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,19 @@
-// const root = document.documentElement;
-// const savedTheme = localStorage.getItem("theme");
+const root = document.documentElement;
+const savedTheme = localStorage.getItem("theme");
 
-// if (savedTheme) {
-//   // user เคยเลือกเอง
-//   root.dataset.theme = savedTheme;
-// } else {
-//   // default = ตามระบบ
-//   const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
-//     ? "dark"
-//     : "light";
-//   root.dataset.theme = systemTheme;
-// }
+if (savedTheme) {
+  // user เคยเลือกเอง
+  root.dataset.theme = savedTheme;
+} else {
+  // default = ตามระบบ
+  const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light";
+  root.dataset.theme = systemTheme;
+}
 
-// document.getElementById("toggleTheme").onclick = () => {
-//   const newTheme = root.dataset.theme === "dark" ? "light" : "dark";
-//   root.dataset.theme = newTheme;
-//   localStorage.setItem("theme", newTheme);
-// };
+document.getElementById("toggleTheme").onclick = () => {
+  const newTheme = root.dataset.theme === "dark" ? "light" : "dark";
+  root.dataset.theme = newTheme;
+  localStorage.setItem("theme", newTheme);
+};
